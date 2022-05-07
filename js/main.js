aaf31444b6bf2658162221e97a9b129e29a69fcd
akramjonnuriddinov <akramjonmohirdev@gmail.com>
2022-05-07 20:50:48 +0500

    Js kodni sug'urtalaba qo'yildi

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 'use strict';
-
-const elNotificationBtn = document.querySelector('.notification-btn');
 const elModal = document.querySelector('.modal');
 const elCloseBtn = document.querySelector('.js-close-btn');
 
-elNotificationBtn.addEventListener('click', function () {
-    elModal.classList.add('modal-show');
-});
-
-elCloseBtn.addEventListener('click', function () {
-    elModal.classList.remove('modal-show');
-});
+if (elCloseBtn) {
+    elCloseBtn.addEventListener('click', function () {
+        elModal.classList.add('modal-close');
+    });
+}
